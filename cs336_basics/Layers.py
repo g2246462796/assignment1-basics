ca0f71f465c4cbaaf71bf3e2e77e1604cdd79a30
Orion <gch2246462796@163.com>
2026-06-04 09:36:51 +0800

    LayerNorm 和 RMSNorm

--- cs336_basics/Layers.py
+++ cs336_basics/Layers.py
@@ -54,3 +54,76 @@ class Embedding(nn.Module):
         # token_ids 形状是 [B, S]，里面每个值是 0 到 vocab_size-1 之间的整数。PyTorch 对每个整数去 self.weight 里取对应那一行，所以输出形状变成 [B, S, embedding_dim]
         return self.weight[token_ids] # 返回形状: [B, S, D]
     
+class LayerNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype= None):
+        """
+        LayerNorm 的手动实现
+        与 RMS Norm相比, 它同时处理了均值 (Mean) 和方差 (Variance) 。
+        """
+        super().__init__()
+        factory_kwargs = {'device': device, 'dtype': dtype}
+
+        # 1. 学习参数初始化
+        # weight (gamma): 缩放参数, 初始化为全 1
+        self.weight = nn.Parameter(torch.ones(d_model, **factory_kwargs))
+        # bias (beta): 偏移参数, 初始化为全0
+        # 这是 LayerNorm 独有的, RMSNorm 通常不使用 bias
+        self.bias = nn.Parameter(torch.zeros(d_model, **factory_kwargs))
+
+        self.eps = eps
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x 形状: (batch_size, sequence_length, d_model)
+
+        in_dtype = x.dtype
+        # 2. 转换为 float32 以确保计算均值和方差时的数值稳定性 (防止溢出)
+        x_float = x.to(torch.float32)
+
+        # 3. 计算均值 (Mean)
+        # 对最后一个维度 (特征维) 求平均, keepdim=True 用于后续减法广播
+        # 公式: E[x]
+        mean = x_float.mean(dim=-1, keepdim=True)
+
+        # 4. 计算方差 (Variance)
+        # 公式: Var(x) = E[(x - E[x])^2]
+        # 注意: 这里使用 biased variance, 与 PyTorch 官方 nn.LayerNorm 对齐
+        var = x_float.var(dim=-1, keepdim=True, unbiased=False)
+
+        # 5. 归一化(Standardization)
+        # 减去均值进行“中心化”，除以标准差进行缩放
+        # 公式：(x - mean) / sqrt(var + eps)
+        x_normed = (x_float - mean) / torch.sqrt(var + self.eps)
+
+        # 6. 应用可学习的增益(weight)和偏置(bias)
+        # 公式: y = x_normed * gamma + beta
+        result = x_normed * self.weight + self.bias
+
+        # 7. 转回输入时的原始数据类型 (如 bfloat16 或 float16)
+        return result.to(in_dtype)
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype= None):
+        super().__init__()
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        # 1. 必须初始化为全 1 (ones)
+        self.weight = nn.Parameter(torch.ones(d_model, **factory_kwargs))
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x : (batch_size, sequence_length, d_model)
+
+        in_dtype = x.dtype
+        # 2. 转换为 float32 以确保计算均值和方差时的数值稳定性 (防止溢出)
+        x_float = x.to(torch.float32)
+
+        # 3. 计算均方根 (Root Mean Square)
+        # 公式: rms = sqrt( mean(x^2) + eps )
+        # dim=-1 表示在隐藏层维度计算, keepdim=True 方便后续除法自动广播
+
+        ms = x_float.pow(2).mean(dim=-1, keepdim=True)
+        rms = torch.sqrt(ms + self.eps)
+
+        # 4. 归一化并乘以可学习的增益函数 g
+        result = (x_float / rms) * self.weight
+
+        # 5. 转回原始类型
+        return result.to(in_dtype)
